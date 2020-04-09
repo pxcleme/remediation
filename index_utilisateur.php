9bfdc69d3e4a1e5666520e5abcb5751e917fc2dd
pxcleme <55398201+pxcleme@users.noreply.github.com>
2020-04-09 09:37:43 +0200

    Add files via upload

---
+++ index_utilisateur.php
@@ -0,0 +1,24 @@
+<?php
+class identifiant
+{
+
+    private $_ID_admin;
+    private $_MDP;
+    private $_Identifiant;
+
+public function inscription($Identifiant,$MDP){
+    try
+    {
+        //execution du code sur la BDD 
+        $BDD = new PDO('mysql:host=localhost; dbname=livresdevoir; charset=utf8','root','');
+        $inscription = $BDD->query('INSERT INTO `inscription` (`Identifiant`, `MDP`) VALUES ("'.$Identifiant.'","'.$MDP.'")');
+    
+    }
+
+    catch (Exception $erreur)
+    {
+        echo 'Erreur : '.$erreur->getmessage();
+    }
+}
+}
+?>
